75472f638d7197ac4c6768baa769bc8f9d87c327
Marjori Pomarole <marjoripomarole@gmail.com>
2013-10-22 15:08:43 -0200

    Adjusting site name test to use rootScope

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -11,12 +11,20 @@ describe('Controller: AppController', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
+    rootScope = $rootScope;
     AppController = $controller('AppController', {
-      $scope: scope
+      $scope: scope,
+      $rootScope: rootScope
     });
   }));
 
-  it('shold have the site name on the scope', function () {
-    expect(scope.site.name).toBe('Atados - Juntando gente Boa');
+  it('shold have the site name on the root scope', function () {
+    expect($rootScope.site.name).toBe('Atados - Juntando gente Boa');
+    expect($rootScope.site.title).toBe('Juntando gente Boa');
   });
 });
+
+// Stuff that I need to test
+  // 1 . I need to make sure the authentication page creates a user well
+  // 2. that the user is correctly authenticated and logged in
+  // 3. That no trace of the user is cached on the client and on the server when he log outs
