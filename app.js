4c9046d6833650351a8053b4fca461e814a68ce6
Marjori Pomarole <marjoripomarole@gmail.com>
2013-12-03 11:20:00 -0200

    Right CSS build

---
+++ app.js
@@ -0,0 +1,21 @@
+ var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 9000);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(app.router);
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
