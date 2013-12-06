daf83e8c48d18247795818a4dd216c46065fcc13
Marjori Pomarole <marjoripomarole@gmail.com>
2013-12-05 22:13:32 -0200

    build

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -16,5 +16,5 @@ var constants = {
   'static_page_size': 50
 };
 
-constants.api = constants.devApi;
-constants.storage = constants.devStorage;
+constants.api = constants.prodApi;
+constants.storage = constants.prodStorage;
