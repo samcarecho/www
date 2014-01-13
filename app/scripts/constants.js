05141fb8e9e72a669b0c7e54f0f932e932513c16
Marjori Pomarole <marjoripomarole@gmail.com>
2014-01-13 12:26:28 -0200

    new dist with prod constants

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -30,5 +30,5 @@ var constants = {
   ]
 };
 
-constants.api = constants.devApi;
-constants.storage = constants.devStorage;
+constants.api = constants.prodApi;
+constants.storage = constants.prodStorage;
