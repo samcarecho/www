86e520281d9dffdb074a9a541cdcc1d78d32ced0
Marjori Pomarole <marjoripomarole@gmail.com>
2014-02-03 16:20:18 -0200

    Fixing resolve nonprofit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -54,9 +54,9 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
       templateUrl: '/partials/nonprofitProfile.html',
       controller: 'NonprofitCtrl',
       resolve: {
-        nonprofit: function (Nonprofit, $stateParams) {
+        nonprofit: ['Nonprofit', '$stateParams', function (Nonprofit, $stateParams) {
           return Nonprofit.get($stateParams.slug);
-        },
+        }]
       }
     })
     .state('root.nonprofitadmin', {
