0ff74626662a83c3d830fa2630fc5ae73e1097af
Marjori Pomarole <marjoripomarole@gmail.com>
2014-08-15 09:25:51 -0300

    Adding dist back on baby

---
+++ dist/public/scripts/constants.js
@@ -0,0 +1,101 @@
+(function(angular, undefined) {
+	 angular.module("atadosConstants", [])
+
+.constant("ENV", "production")
+
+.constant("api", "https://api.atados.com.br/v1/")
+
+.constant("authApi", "https://www.atados.com.br/auth/client")
+
+.constant("storage", "https://s3-sa-east-1.amazonaws.com/atadosapp/images/")
+
+.constant("selected", "https://s3-sa-east-1.amazonaws.com/atadosapp/images/heart.png")
+
+.constant("notselected", "https://s3-sa-east-1.amazonaws.com/atadosapp/images/blue.png")
+
+.constant("facebookClientId", "430973993601792")
+
+.constant("locale", "pt_BR")
+
+.constant("accessTokenCookie", "access_token")
+
+.constant("csrfCookie", "csrftoken")
+
+.constant("sessionIdCookie", "sessionid")
+
+.constant("grantType", "password")
+
+.constant("page_size", 30)
+
+.constant("active_cities", 4)
+
+.constant("static_page_size", 300)
+
+.constant("weekdays", [
+  {
+    "1": "Segunda"
+  },
+  {
+    "2": "Terça"
+  },
+  {
+    "3": "Quarta"
+  },
+  {
+    "4": "Quinta"
+  },
+  {
+    "5": "Sexta"
+  },
+  {
+    "6": "Sabado"
+  },
+  {
+    "7": "Domingo"
+  }
+])
+
+.constant("periods", [
+  {
+    "0": "Manha"
+  },
+  {
+    "1": "Tarde"
+  },
+  {
+    "2": "Noite"
+  }
+])
+
+.constant("defaultZoom", 10)
+
+.constant("VOLUNTEER", "VOLUNTEER")
+
+.constant("NONPROFIT", "NONPROFIT")
+
+.constant("saoPaulo", {
+  "id": 9422,
+  "lat": -23.5505199,
+  "lng": -46.6333094
+})
+
+.constant("curitiba", {
+  "id": 5915,
+  "lat": -25.4808762,
+  "lng": -49.3044253
+})
+
+.constant("brasilia", {
+  "id": 1724,
+  "lat": -15.79211,
+  "lng": -47.897751
+})
+
+.constant("distancia", {
+  "id": 0,
+  "lat": 0,
+  "lng": 0
+})
+
+; 
+})(angular);
